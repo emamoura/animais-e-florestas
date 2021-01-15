987f3372c09e8c6c39b0c17af3f148bcfe94384e
emamoura <56855456+emamoura@users.noreply.github.com>
2021-01-15 19:01:39 -0300

    Atualização JQuery, HTML e CSS

--- js/app.js
+++ js/app.js
@@ -1,27 +1,98 @@
-$(document).ready(function(){
-	var classActive = 'active';
+// Mudat tab ao click
 
-	$('.animais .tab-menu a').first().addClass(classActive);
-	$('.animais .item').first().addClass(classActive);
+$('[data-group]').each(function () {
+	var $allTarget = $(this).find('[data-target]'),
+		$allClick = $(this).find('[data-click]'),
+		activeClass = 'active';
 
-	$('.animais .tab-menu a').click(function(e){
+	$allTarget.first().addClass(activeClass);
+	$allClick.first().addClass(activeClass);
+
+	$allClick.click(function (e) {
 		e.preventDefault();
-		var itemId = $(this).attr('href');
 
-		$('.animais .tab-menu a, .animais  .item').removeClass(classActive);
-		$(this).addClass(classActive);
-		$(itemId).addClass(classActive);
-	});
+		var id = $(this).data('click'),
+			$target = $('[data-target="' + id + '"]');
 
-	$('.florestas .tab-menu a').first().addClass(classActive);
-	$('.florestas .item').first().addClass(classActive);
+		$allClick.removeClass(activeClass);
+		$allTarget.removeClass(activeClass);
 
-	$('.florestas .tab-menu a').click(function(e){
-		e.preventDefault();
-		var itemId = $(this).attr('href');
-
-		$('.florestas .tab-menu a, .florestas  .item').removeClass(classActive);
-		$(this).addClass(classActive);
-		$(itemId).addClass(classActive);
+		$target.addClass(activeClass);
+		$(this).addClass(activeClass);
 	});
 });
+
+// Scroll suave para link interno
+$('.menu-nav a[href^="#"]').click(function (e) {
+	e.preventDefault();
+	var id = $(this).attr('href'),
+		menuHeight = $('.menu').innerHeight(),
+		targetOffset = $(id).offset().top;
+	$('html, body').animate({
+		scrollTop: targetOffset - menuHeight
+	}, 500);
+});
+
+//Scroll suave para o topo
+
+$('.logo').click(function (e) {
+	e.preventDefault();
+	$('html, body').animate({
+		scrollTop: 0
+	}, 500)
+});
+
+// Mudar para active o menu de acordo com a área
+
+$('section').each(function () {
+	var height = $(this).height(),
+		offsetTop = $(this).offset().top,
+		menuHeight = $('.menu').innerHeight(),
+		id = $(this).attr('id'),
+		$itemMenu = $('a[href="#' + id + '"]');
+
+	$(window).scroll(function () {
+		var scrollTop = $(window).scrollTop();
+		if (offsetTop - menuHeight < scrollTop && offsetTop + height - menuHeight > scrollTop) {
+			$itemMenu.addClass('active');
+		} else {
+			$itemMenu.removeClass('active');
+		}
+	});
+});
+
+//Botão do menu	mobile
+
+$('.mobile-btn').on('click', function () {
+	$(this).toggleClass('active');
+	$('.mobile-menu').toggleClass('active');
+});
+
+//Slider
+
+function slider(sliderName, velocidade) {
+
+	var sliderClass = '.' + sliderName,
+		activeClass = 'active';
+		rotate = setInterval(rotateSlide, velocidade);
+		$(sliderClass + ' > :first').addClass(activeClass);
+
+	$(sliderClass).hover(function(){
+		clearInterval(rotate);
+	}, function(){
+		rotate = setInterval(rotateSlide, velocidade);
+	});
+
+	function rotateSlide() {
+		var activeSlide = $(sliderClass + ' > ' + '.active'),
+			nextSlide = activeSlide.next();
+		if (nextSlide.length === 0) {
+			nextSlide = $(sliderClass + ' > :first');
+		}
+
+		activeSlide.removeClass(activeClass);
+		nextSlide.addClass(activeClass);
+	}
+
+} 
+slider('introducao', 2000)
